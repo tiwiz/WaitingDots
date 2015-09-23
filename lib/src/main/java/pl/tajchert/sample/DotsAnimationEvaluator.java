5c2aaa20cb0b41b30dd66658639e48c179d411b4
Roberto Orgiu <roberto.orgiu@gmail.com>
2015-09-23 07:28:24 +0200

    Adds LeakCanary to check the leak in the library

---
+++ lib/src/main/java/pl/tajchert/sample/DotsAnimationEvaluator.java
@@ -0,0 +1,7 @@
+package pl.tajchert.sample;
+
+/**
+ * Created by tiwiz on 23/09/15.
+ */
+public class DotsAnimationEvaluator {
+}
---
+++ sample/src/main/java/pl/tajchert/waitingdots/sample/SampleApplication.java
@@ -0,0 +1,17 @@
+package pl.tajchert.waitingdots.sample;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ *
+ */
+public class SampleApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
+}
